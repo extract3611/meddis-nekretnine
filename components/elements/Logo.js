0c50e656cd2a790329adc7939b3984550e4d3e18
extract36111 <digitalartefakt361@gmail.com>
2024-12-18 16:48:44 +0100

    share

--- components/elements/Logo.js
+++ components/elements/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="brand-logo">
       <Link href="/">
-        <img  src="/assets/images/logo.svg" alt="" className="img-fluid" />
+        <img  src="./assets/images/logo.svg" alt="" className="img-fluid" />
       </Link>
     </div>
   );
--- pages/nekretnine/nekretnina/[nekretnina].js
+++ pages/nekretnine/nekretnina/[nekretnina].js
@@ -1,95 +1,67 @@
-/**
- * It fetches data from the API and then passes it to the component
- * @returns The data is being returned as an array of objects.
- */
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { ucitajNekretninu } from "../../../redux-toolkit/nekretnine-redux/izabranaNekretnina";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import BodyContent from "../../../components/property/tabPanelPages";
-//import { LoadingScreen } from "../../../components/Common/loader";
-
+import Head from 'next/head';
 import Layout from "../layout";
-import { useSelector,useDispatch } from "react-redux";
-import { slide1 } from "../../../data/slickSlider";
-export const getStaticProps = async ({ locale }) => ({ props: { ...(await serverSideTranslations(locale, ["common"])) } });
-let p;
-export const getStaticPaths = async ({ locales }) => {
-  const response = await axios.get(`https://white-homes.me/api/nekretnine`);
-  const data = response.data;
-  
 
-
-
-  const ids = data.map((nekretnina) => nekretnina.id_nekretnina);
-  const paths = ids
-    .map((id) =>
-      locales.map((locale) => ({
-        params: { nekretnina: id.toString() },
-        locale, //locale should not be inside `params`
-      }))
-    )
-    .flat(); // to avoid nested arrays
+// Get server-side props to fetch data before the page is rendered
+export const getServerSideProps = async ({ params, locale }) => {
+  const response = await axios.get(`https://white-homes.me/api/nekretnine/${params.nekretnina}`);
+  const nekretnina = response.data;
 
   return {
-    paths,
-    fallback: true,
+    props: {
+      nekretnina,
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
   };
 };
 
-const ImageBox = () => {
-const router = useRouter();
+const ImageBox = ({ nekretnina }) => {
+  const [slike, setSlike] = useState([]);
 
-  const query = router.query.nekretnina;
+  useEffect(() => {
+    if (nekretnina) {
+      // Fetch gallery images
+      axios.post(`/api/galerija`, { id: nekretnina.id_nekretnina })
+        .then(response => setSlike(response.data))
+        .catch(error => console.error(error));
+    }
+  }, [nekretnina]);
 
-useEffect(()=>{console.log(query)},[query])
-  const nekretnine=useSelector(state=>state.nekretnine.niz)
-  const nekretninaa=useSelector(state=>state.izabranaNekretnina)
+  // Ensure nekretnina is loaded before rendering the page
+  if (!nekretnina) {
+    return <div>Učitavanje...</div>; // Show loading state while fetching data
+  }
 
-  const [nekretnina,postaviNekretninu]=useState(null);
-  const [slike,postaviSlike]=useState([]);
-  const [loader,postaviLoader]=useState(false);
-  
-
-  useEffect(()=>console.log(slike),[slike])
-
-  const dispatch=useDispatch();
-
-
-  useEffect(()=>{
-    if(nekretnine.length!=0 && query!=null){
-      console.log('aaa')
-    console.log(nekretnine)
-    postaviNekretninu(nekretnine.filter(x=>x.id_nekretnina==query))
-    console.log(nekretnine.filter(x=>x.id_nekretnina==query))
-    dispatch(ucitajNekretninu(nekretnine.filter(x=>x.id_nekretnina==query)[0]));
-  }},[query])
-  useEffect(()=>{
-    postaviLoader(true)
-  },[nekretninaa])
-  if(nekretninaa!=null){
-    console.log(nekretninaa)
-    if(loader){
   return (
     <>
-      <BodyContent singleData={nekretninaa}>
-      </BodyContent>
+      <Head>
+        <title>{nekretnina.naziv}</title>
+        <meta property="og:title" content={nekretnina.naziv} />
+        <meta property="og:description" content={nekretnina.opis} />
+        {/* Dynamic Open Graph image URL for individual property */}
+        <meta property="og:image" content={nekretnina.slika || "https://default-image-url.com/default.jpg"} />
+        <meta property="og:url" content={`https://www.meddis-nekretnine.me/nekretnine/nekretnina/${nekretnina.id_nekretnina}`} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={nekretnina.naziv} />
+        <meta name="twitter:description" content={nekretnina.opis} />
+        <meta name="twitter:image" content={nekretnina.slika || "https://default-image-url.com/default.jpg"} />
+      </Head>
+      <BodyContent slike={slike} singleData={nekretnina} />
     </>
   );
-}}
-else{return(<><h1>ocitavanje</h1></>)}
-
-}
-
-ImageBox.getLayout = function getLayout(page, nekretninaa = null) {
-  const title = nekretninaa
-    ? `Meddis nekretnine - ${nekretninaa.naziv}`
-    : "Meddis nekretnine";
-  const description = nekretninaa
-    ? `Meddis nekretnine - ${nekretninaa.naziv}, površina ${nekretninaa.povrsina} m², ${nekretninaa.brojSoba} spavaće sobe. ${nekretninaa.opis}. Pogledajte više detalja i cijenu na našoj stranici!`
-    : "Kupovina i prodaja nekretnina";
-
-  return <Layout title={title} description={description}>{page}</Layout>;
 };
+
+ImageBox.getLayout = function getLayout(page) {
+  return (
+    <Layout title="Sve nekretnine - Najveća baza nekretnina u Crnoj Gori" description="Bez obzira da li tražite nekretninu za investiciju, stanovanje ili odmor, mi vam nudimo široki spektar svih tipova nekretnina.">
+      {page}
+    </Layout>
+  );
+};
+
 export default ImageBox;
